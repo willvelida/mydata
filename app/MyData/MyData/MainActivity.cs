1723f664c158586fc17366f9a4ffa38db91d6aa3
Will Velida <willvelida@hotmail.co.uk>
2017-12-18 14:14:52 +0000

    Adding Workout Log Xamarin app

---
+++ app/MyData/MyData/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace MyData
+{
+    [Activity(Label = "MyData", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get UI Controls from layout
+            Button newWorkoutButton = FindViewById<Button>(Resource.Id.newExerciseButton);
+
+            //
+            newWorkoutButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(NewWorkout));
+                StartActivity(intent);
+            };
+
+
+        }
+    }
+}
+
---
+++ app/MyData/MyData/NewWorkout.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MyData
+{
+    [Activity(Label = "NewWorkout")]
+    public class NewWorkout : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.NewExercise);
+
+            // Set UI Elements
+            EditText exerciseDate = FindViewById<EditText>(Resource.Id.exerciseDate);
+            LinearLayout weightsLayout = FindViewById<LinearLayout>(Resource.Id.weightsLayout);
+            EditText weightName = FindViewById<EditText>(Resource.Id.weightName);
+            EditText weightSets = FindViewById<EditText>(Resource.Id.weightSets);
+            EditText weightReps = FindViewById<EditText>(Resource.Id.weightReps);
+            LinearLayout cardioLayout = FindViewById<LinearLayout>(Resource.Id.cardioLayout);
+            EditText cardioName = FindViewById<EditText>(Resource.Id.cardioName);
+            EditText cardioDuration = FindViewById<EditText>(Resource.Id.cardioDuration);
+            Button addWeightButton = FindViewById<Button>(Resource.Id.addNewWeightExerciseButton);
+            Button addCardioButton = FindViewById<Button>(Resource.Id.addNewCardioExerciseButton);
+            Button submitExerciseButton = FindViewById<Button>(Resource.Id.SubmitExerciseButton);
+
+
+            submitExerciseButton.Click += (sender, e) =>
+            {
+                // Create new intent
+                var intent = new Intent(this, typeof(MainActivity));
+                // TODO: Submit new data to DB
+                // Go back to Main activity
+                StartActivity(intent);
+            };
+            
+        }
+    }
+}
