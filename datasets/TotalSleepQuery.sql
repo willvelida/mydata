c9df2e681b6094f7c6a5ff1e53e6ff9c1bef3c9d
Will Velida <willvelida@hotmail.co.uk>
2017-12-29 20:40:37 +0000

    Adding sleep query

---
+++ datasets/TotalSleepQuery.sql
@@ -0,0 +1,23 @@
+SELECT *
+FROM TotalSleep;
+
+ALTER TABLE [MyDataDB].[dbo].[TotalSleep]
+ALTER COLUMN [Minutes Asleep] int;
+
+ALTER TABLE [MyDataDB].[dbo].[TotalSleep]
+ALTER COLUMN [Minutes Awake] int;
+
+ALTER TABLE [MyDataDB].[dbo].[TotalSleep]
+ALTER COLUMN [Number of Awakenings] int;
+
+ALTER TABLE [MyDataDB].[dbo].[TotalSleep]
+ALTER COLUMN [Time in Bed] int;
+
+ALTER TABLE [MyDataDB].[dbo].[TotalSleep]
+ALTER COLUMN [Minutes REM Sleep] int;
+
+ALTER TABLE [MyDataDB].[dbo].[TotalSleep]
+ALTER COLUMN [Minutes Light Sleep] int;
+
+ALTER TABLE [MyDataDB].[dbo].[TotalSleep]
+ALTER COLUMN [Minutes Deep Sleep] int;
